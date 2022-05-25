3ea2bdf9811b663365e63f66ee02d5d8c9dc207a
Sha <charlotte.megroff@outlook.com>
2022-05-26 00:38:36 +0100

    navbar selection

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,14 +2,25 @@ import '../App.css';
 import CVpdf from '../Docs/cvtex.pdf';
 
 //Commented out for Netlify
-// import { useState } from 'react';
+import { useState } from 'react';
+
 
 export const Navbar = () => {
+    const [clickState, setClickState] = useState([1, 0, 0, 0]);
+    const elementClassLookupArray = ["headerBarMenuElement", "headerBarMenuElementBold"];
     //Commented out for Netlify
     // const [navHover, setNavHover] = useState(false);
     // const [homeClicked, setHomeClicked] = useState(false);
     // const [skillsClicked, setSkillsClicked] = useState(false);
     // const [experienceClicked, setExperienceClicked] = useState(false);
+
+    // <p className = "rightMenuElement" onMouseOver = {() => {setLinkedinHover(1)}} onMouseOut = {() => {setLinkedinHover(0)}}><a href="https://www.linkedin.com/in/charlotte-megroff-88aa17176/"><img src={logoLookup["linkedin"][linkedinHover]} alt="LinkedIn"/>LinkedIn</a></p>
+
+    const boldHandler = (elementIndex) => {
+        let newArray = [0, 0, 0, 0];
+        newArray[elementIndex] = 1;
+        setClickState(newArray);
+    }
 
     return (
             <div className = "headerBar150px" id = "headAll">
@@ -21,13 +32,13 @@ export const Navbar = () => {
 
                     </div>
                     <div className = "headerBarMenuInner2">
-                        <div className = "headerBarMenuElement">Home</div>
+                        <div onClick = {() => {boldHandler(0)}} className = {elementClassLookupArray[clickState[0]]}>Home</div>
                         <div>•</div>
-                        <a href={CVpdf}><div className = "headerBarMenuElement">CV</div></a>
+                        <a href={CVpdf}><div className = {elementClassLookupArray[clickState[1]]}>CV</div></a>
                         <div>•</div>
-                        <div className = "headerBarMenuElement">Skills</div>
+                        <div onClick = {() => {boldHandler(2)}} className = {elementClassLookupArray[clickState[2]]}>Skills</div>
                         <div>•</div>
-                        <div className = "headerBarMenuElement">Experience</div>
+                        <div onClick = {() => {boldHandler(3)}} className = {elementClassLookupArray[clickState[3]]}>Experience</div>
                         {/* this needs to migrate when page narrow */}
                     </div>
                 </div>
