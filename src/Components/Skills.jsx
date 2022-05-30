d2bab193a8a33290e294ae72777eb696e5352369
Sha <charlotte.megroff@outlook.com>
2022-05-30 21:57:38 +0100

    Skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -2,15 +2,30 @@ import '../App.css';
 
 export const Skills = () => {
     return (
-        
         <div className = "bodyAreaPage">
-            <div className ="bodyAreaPageTitle">Skills</div>
+            <div>
+                <div className ="bodyAreaPageSkillsTitle">Skills</div>
+            </div>
+
+            <div>
+
+            <div className = "bodyAreaPageMainText">
+                <div className ="bodyAreaPageMainInitial">M</div>
+                <div className ="bodyAreaPageMainInnerBody">y coding skills broadly fall into three categories. There are technologies I've had most of my experience with through full-time education, those I've picked up as part of my working career and those I've learned through self-study as a hobbyist.</div>
+            </div>
+
             <div className = "bodyAreaPageMainText2">
 
-            <div className ="bodyAreaPageMainInnerBody2"> <p>Under construction, please see my CV for details.</p>
+                <div className ="bodyAreaPageMainInnerBody2"> <p>My most recent study was with the 12-week Code Nation boot camp where we focussed on modern JavaScript web development using the MERN stack; MongoDB, Express, React and Node. Together this made up a full-stack course which culminated in my team building a full-stack MERN web app using MongoDB for the back end and React for the front end. We hosted this on Netlify at <a href="https://charlotte-megroff.netlify.app/">https://charlotte-megroff.netlify.app/</a></p>
+
+                <p></p>
+                </div>
+                
             </div>
 
+            
             </div>
+
         </div>
     )
 }
