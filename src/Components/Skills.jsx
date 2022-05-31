f0f6857e4cd8824c67151b3881977490dede18bf
Sha <charlotte.megroff@outlook.com>
2022-05-31 12:19:11 +0100

    Skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -16,9 +16,10 @@ export const Skills = () => {
 
             <div className = "bodyAreaPageMainText2">
 
-                <div className ="bodyAreaPageMainInnerBody2"> <p>My most recent study was with the 12-week Code Nation boot camp where we focussed on modern JavaScript web development using the MERN stack; MongoDB, Express, React and Node. Together this made up a full-stack course which culminated in my team building a full-stack MERN web app using MongoDB for the back end and React for the front end. We hosted this on Netlify at <a href="https://charlotte-megroff.netlify.app/">https://charlotte-megroff.netlify.app/</a></p>
+                <div className ="bodyAreaPageMainInnerBody2"> <p>My most recent study was with the 12-week Code Nation boot camp where we focussed on modern JavaScript web development using the MERN stack; MongoDB, Express, React and Node. Together this made up a full-stack course which culminated in my team building a full-stack MERN web app using MongoDB for the back end, including user registration, and React for the front end. We hosted this on Netlify at <a href="https://view-finder.netlify.app/">https://view-finder.netlify.app/</a>.</p>
+                <p>Previously in my career I've worked for an insurer using Visual Basic for Applications to reassemble and reconcile large volumes of accounting data, and built Excel tools for the Co-Operative Group's project management team.</p>
 
-                <p></p>
+                <p>As a hobbyist I began programming with Python and later moved on to C++. My most recent project was a clock and system monitor for an LCD screen on my home PC.</p>
                 </div>
                 
             </div>
